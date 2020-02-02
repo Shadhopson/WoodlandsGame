03fb44c70f5623a29fd935ae5ffb54b7bbdfe2e0
Shadhopson <hopson.shad@gmail.com>
2020-02-02 12:43:40 -0500

    off by one fix

--- sketch.js
+++ sketch.js
@@ -227,7 +227,7 @@ for (var i =0; i < 25; i++){
     selected_creatures.push(randnum);
   }
 }
-selected_creatures = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
+selected_creatures = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
 
 function setup() {
   photos = [];
@@ -356,7 +356,7 @@ else{
       mouseY > next_box['top'] && mouseY < next_box['top'] + next_box['height']){
         answered = false;
         current_animal +=1;
-        if (current_animal >= selected_creatures.length){
+        if (current_animal >= selected_creatures.length-1){
           current_animal = 0;
         }
         /*if(current_animal >= 24){
